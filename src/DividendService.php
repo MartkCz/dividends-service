4e36804f30ba96eb2ccf91e244801fb9893db398
MartkCz <martk494@gmail.com>
2024-09-11 12:32:26 +0200

    feat(core): use api-core

--- src/DividendService.php
+++ src/DividendService.php
@@ -2,89 +2,28 @@
 
 namespace Api\Dividends;
 
+use Api\Core\RequestType;
+use Api\Core\Service;
+use Api\Core\ServiceRequest;
 use App\Portfolio\Transaction;
-use DateTimeInterface;
-use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Contracts\HttpClient\ResponseInterface;
 
 /**
  * @phpstan-type Transaction array{ symbol: string, shares: float, investment: float, date: string }
  */
-final class DividendService
+final class DividendService extends Service
 {
 
 	public const PortfolioLink = '/dividends/portfolio';
 
-	private HttpClientInterface $httpClient;
-
-	public function __construct(
-		private string $baseUrl,
-		?HttpClientInterface $httpClient,
-	)
-	{
-		$this->httpClient = $httpClient ?? HttpClient::create();
-	}
-
 	/**
 	 * @param Transaction[] $transactions
 	 */
-	public function requestPortfolio(
-		array $transactions,
-		int $year,
-		?DateTimeInterface $portfolioLastUpdate = null,
-		?DateTimeInterface $ifModifiedSince = null,
-		?string $ifNoneMatch = null,
-		bool $sensitive = true,
-	): ResponseInterface
+	public function portfolio(array $transactions, int $year, bool $sensitive): ServiceRequest
 	{
-		return $this->httpClient->request('POST', $this->buildUrl(self::PortfolioLink, [
+		return $this->requestJson(RequestType::Post, $transactions, self::PortfolioLink, [
 			'year' => $year,
 			'sensitive' => $sensitive ? null : 'false',
-		]), [
-			'json' => $transactions,
-			'headers' => $this->createHeaders($portfolioLastUpdate, $ifModifiedSince, $ifNoneMatch),
 		]);
-	}
-
-	/**
-	 * @param array<string, scalar> $params
-	 */
-	private function buildUrl(string $path, array $params = []): string
-	{
-		$url = $this->baseUrl . $path;
-
-		if (count($params) > 0) {
-			$url .= '?' . http_build_query($params);
-		}
-
-		return $url;
-	}
-
-	/**
-	 * @return array<string, string>
-	 */
-	private function createHeaders(
-		?DateTimeInterface $lastUpdate,
-		?DateTimeInterface $ifModifiedSince,
-		?string $ifNoneMatch,
-	): array
-	{
-		$headers = [];
-
-		if ($lastUpdate) {
-			$headers['X-Last-Update'] = $lastUpdate->format('D, d M Y H:i:s \G\M\T');
-		}
-
-		if ($ifModifiedSince) {
-			$headers['If-Modified-Since'] = $ifModifiedSince->format('D, d M Y H:i:s \G\M\T');
-		}
-
-		if ($ifNoneMatch) {
-			$headers['If-None-Match'] = $ifNoneMatch;
-		}
-
-		return $headers;
 	}
 
 }
